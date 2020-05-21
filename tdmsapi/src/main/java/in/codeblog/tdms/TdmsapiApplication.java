6e7536047b2968acff74d938e2bd5f6e6a8d94c4
Sharma Pankaj <sharma.pankaj@Yash.com>
2020-05-21 13:18:05 +0530

    Initial Commit for backend code

---
+++ tdmsapi/src/main/java/in/codeblog/tdms/TdmsapiApplication.java
@@ -0,0 +1,13 @@
+package in.codeblog.tdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdmsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdmsapiApplication.class, args);
+	}
+
+}
---
+++ tdmsapi/src/test/java/in/codeblog/tdms/TdmsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.codeblog.tdms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdmsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
