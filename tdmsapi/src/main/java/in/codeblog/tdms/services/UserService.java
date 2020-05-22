2b9b424e0bfd6f527cb5594833cbff334fe048c0
ajeetgithub2020@gmail.com <ajeetgithub2020@gmail.com>
2020-05-22 13:09:48 +0530

    getPendingUserRequests added in UserService

--- tdmsapi/src/main/java/in/codeblog/tdms/services/UserService.java
+++ tdmsapi/src/main/java/in/codeblog/tdms/services/UserService.java
@@ -1,5 +1,26 @@
 package in.codeblog.tdms.services;
-
+import java.util.LinkedList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import in.codeblog.tdms.domain.User;
+import in.codeblog.tdms.reposiorities.UserRepository;
+@Service
 public class UserService {
-
+@Autowired
+private UserRepository userRepository;
+public List<User>getPendingUserRequests()
+{
+	List<User>users=userRepository.findAll();
+	List<User>toSend=new LinkedList<>();
+	for(User user:users)
+	{
+		if(user.getRequest()==0)
+		{
+			toSend.add(user);
+		}
+	}
+	users=null;
+return toSend;
+}
 }
